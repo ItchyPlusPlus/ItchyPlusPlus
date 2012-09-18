26542ac69eea89b4220acf5c7b237fa88ad256d6
MathWizz <mathwizz94@gmail.com>
2012-09-18 16:25:20 -0500

    Starting file reading

--- src/scratchio.cpp
+++ src/scratchio.cpp
@@ -6,67 +6,125 @@ ByteStream::ByteStream(istream* stream) {
 
 uint8_t ByteStream::uint8() {
 	uint8_t n;
-	this->readBlock(&n, sizeof(uint8_t));
+	this->readBlockR(&n, sizeof(uint8_t));
 	return n;
 }
 
 uint16_t ByteStream::uint16() {
 	uint16_t n;
-	this->readBlock(&n, sizeof(uint16_t));
+	this->readBlockR(&n, sizeof(uint16_t));
 	return n;
 }
 
 uint32_t ByteStream::uint32() {
 	uint32_t n;
-	this->readBlock(&n, sizeof(uint32_t));
+	this->readBlockR(&n, sizeof(uint32_t));
 	return n;
 }
 
 uint64_t ByteStream::uint64() {
 	uint64_t n;
-	this->readBlock(&n, sizeof(uint64_t));
+	this->readBlockR(&n, sizeof(uint64_t));
 	return n;
 }
 
 int8_t ByteStream::int8() {
 	int8_t n;
-	this->readBlock(&n, sizeof(int8_t));
+	this->readBlockR(&n, sizeof(int8_t));
 	return n;
 }
 
 int16_t ByteStream::int16() {
 	int16_t n;
-	this->readBlock(&n, sizeof(int16_t));
+	this->readBlockR(&n, sizeof(int16_t));
 	return n;
 }
 
 int32_t ByteStream::int32() {
 	int32_t n;
-	this->readBlock(&n, sizeof(int32_t));
+	this->readBlockR(&n, sizeof(int32_t));
 	return n;
 }
 
 int64_t ByteStream::int64() {
 	int64_t n;
-	this->readBlock(&n, sizeof(int64_t));
+	this->readBlockR(&n, sizeof(int64_t));
 	return n;
 }
 
 float ByteStream::float32() {
 	float n;
-	this->readBlock(&n, sizeof(float));
+	this->readBlockR(&n, sizeof(float));
 	return n;
 }
 
 double ByteStream::float64() {
 	double n;
-	this->readBlock(&n, sizeof(double));
+	this->readBlockR(&n, sizeof(double));
 	return n;
+}
+
+char* ByteStream::readString(uint32_t size) {
+	char* string = new char[size];
+	this->readBlock(string, size);
+	return string;
 }
 
 void ByteStream::readBlock(void* block, uint32_t size) {
 	this->stream->read((char*) block, size);
 }
+
+void ByteStream::readBlockR(void* block, uint32_t size) {
+	this->readBlock(block, size);
+	std::reverse((char*) block, (char*) block + size);
+}
+
+
+ScratchReader::ScratchReader(ByteStream* stream) {
+	this->stream = stream;
+}
+
+
+void ScratchReader::readProject() {
+	if (strcmp(this->stream->readString(10), "ScratchV02") != 0) {
+		cout << "scratch project" << endl;
+
+		cout << "info size: " << this->stream->uint32() << endl;
+
+		this->readObjectStore();
+	} else {
+		cout << "not scratch project" << endl;
+	}
+}
+
+void ScratchReader::readObjectStore() {
+	if (strcmp(this->stream->readString(10), "ObjS\0Stch\0") != 0) {
+		cout << "is object" << endl;
+
+		uint32_t size = this->stream->uint32();
+
+		cout << "object size: " << size << endl;
+
+		for (int i = 0; i < size; i++) {
+			this->readObject();
+		}
+	} else {
+		cout << "not object" << endl;
+	}
+}
+
+void ScratchReader::readObject() {
+	uint8_t id = this->stream->uint8();
+	if (id < 99) {
+		readFixedFormat(id);
+	}
+	cout << "id: " << (int) id << endl;
+}
+
+void ScratchReader::readFixedFormat(uint8_t id) {
+
+}
+
 
 void openFromFile(const char* path) {
 	ifstream::pos_type size;
@@ -84,10 +142,6 @@ void openFromFile(const char* path) {
 
 void openFromStream(istream* s) {
 	ByteStream stream(s);
-
-	for (int i = 0; i < 10; i++) {
-		cout << (char) stream.uint8();
-	}
-
-	cout << endl;
+	ScratchReader reader(&stream);
+	reader.readProject();
 }
--- src/scratchio.h
+++ src/scratchio.h
@@ -1,9 +1,11 @@
 #ifndef SCRATCHIO_H__
 #define SCRATCHIO_H__
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
+#include <string.h>
 
 using namespace std;
 
@@ -23,10 +25,31 @@ public:
 	int64_t int64();
 	float float32();
 	double float64();
+
+	char* readString(uint32_t);
+	void readBlock(void* block, uint32_t);
+	void readBlockR(void* block, uint32_t);
 private:
 	istream* stream;
+};
 
-	void readBlock(void* block, uint32_t);
+class ObjectRecord {
+public:
+	uint8_t id;
+	uint8_t version;
+	uint8_t fieldCount;
+};
+
+class ScratchReader {
+public:
+	ScratchReader(ByteStream*);
+	void readProject();
+	void readObjectStore();
+	void readObject();
+	ObjectRecord* readField();
+	void readFixedFormat(uint8_t);
+private:
+	ByteStream* stream;
 };
 
 #endif
