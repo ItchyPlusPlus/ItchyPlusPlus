94d342a34f9534ea613078ecfea63a48edd05857
TRocket <TRocket@Itchypp.github>
2013-02-18 19:57:33 +0000

    image now fills entire window

--- src/mainpane.cpp
+++ src/mainpane.cpp
@@ -12,9 +12,9 @@ MainPane::MainPane(QWidget *parent) :
 
 void MainPane::paintEvent(QPaintEvent *) {
     QPainter painter(this);
-    QImage image(640, 480, QImage::Format_RGB32);
-    for(int x = 1; x < 640; x++) {
-        for(int y = 1; y < 480; y++) {
+    QImage image(this->width(), this->height(), QImage::Format_RGB32);
+    for(int x = 1; x < this->width(); x++) {
+        for(int y = 1; y < this->height(); y++) {
             int r = rand() % (255 - 0 + 1) + 0;
             int g = rand() % (255 - 0 + 1) + 0;
             int b = rand() % (255 - 0 + 1) + 0;
